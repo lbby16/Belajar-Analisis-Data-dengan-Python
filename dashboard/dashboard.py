f8a944844cfe11c2905c11586ab1ea600236bbe8
labib <labibyusufaditama@gmail.com>
2024-05-03 21:03:42 +0700

    notebook change and dashboard

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -37,57 +37,95 @@ if st.sidebar.checkbox("Show Summary Statistics"):
 st.sidebar.markdown("[Download Dataset](https://drive.google.com/drive/folders/1Uk2CDwEwimKPy395dDDqfA8rUc1WCTvI?usp=sharing)")
 
 # VISUALIZATION
-# create a layout with two columns
-col1, col2 = st.columns(2)
-
-with col1:
-    # Number of bicycle shares by season
-    # st.subheader("Number of bicycle shares by season")
-
-    # Mapping dari angka ke label musim
-    season_mapping = {1: "spring", 2: "summer", 3: "fall", 4: "winter"}
-    data["season_label"] = data["season"].map(season_mapping)
-
-    season_count = data.groupby("season_label")["cnt"].sum().reset_index()
-    fig_season_count = px.bar(season_count, x="season_label",
-                              y="cnt", title="Number of bicycle shares by season")
-    st.plotly_chart(fig_season_count, use_container_width=True,
-                    height=400, width=600)
-
-with col2:
-    # Number of Bicycle Rentals Based on Weather Situations
-    # st.subheader("Number of Bicycle Rentals Based on Weather Situations")
-
-    weather_count = data.groupby("weathersit")["cnt"].sum().reset_index()
-    fig_weather_count = px.bar(weather_count, x="weathersit",
-                               y="cnt", title="Number of Bicycle Rentals Based on Weather Situations")
-    # Mengatur tinggi dan lebar gambar
-    st.plotly_chart(fig_weather_count, use_container_width=True,height=400, width=800)
+df_bsd_day = pd.read_csv("data/day.csv")
+df_bsd_hour = pd.read_csv("data/hour.csv")
+# df_bsd_day
+df_bsd_day["dteday"] = pd.to_datetime(df_bsd_day["dteday"])
+# df_bsd_hour
+df_bsd_hour["dteday"] = pd.to_datetime(df_bsd_hour["dteday"])
 
 
-# Hourly bike share count
-# st.subheader("Hourly Bike Share Count")
-hourly_count = data.groupby("hr")["cnt"].sum().reset_index()
-fig_hourly_count = px.line(
-    hourly_count, x="hr", y="cnt", title="Hourly Bike Share Count")
-st.plotly_chart(fig_hourly_count, use_container_width=True,
-                height=400, width=600)
 
-# Humidity vs. Bike Share Count
-# st.subheader("Humidity vs. Bike Share Count")
-fig_humidity_chart = px.scatter(
-    data, x="hum", y="cnt", title="Humidity vs. Bike Share Count")
-st.plotly_chart(fig_humidity_chart)
+st.subheader("Pertanyaan 1")
+# Filter tahun 2011 dan musim dingin (season 4)
+filtered_data = df_bsd_day[(df_bsd_day["yr"] == 0) & (df_bsd_day["season"] == 4)]
 
-# Wind Speed vs. Bike Share Count
-# st.subheader("Wind Speed vs. Bike Share Count")
-fig_wind_speed_chart = px.scatter(
-    data, x="windspeed", y="cnt", title="Wind Speed vs. Bike Share Count")
-st.plotly_chart(fig_wind_speed_chart)
+# Filter tahun 2011 dan musim dingin (season 4)
+# Hitung jumlah total sewa sepeda (cnt)
+total_sewa_sepeda = filtered_data["cnt"].sum()
 
-# Temperature vs. Bike Share Count
-# st.subheader("Temperature vs. Bike Share Count")
-fig_temp_chart = px.scatter(data, x="temp", y="cnt",
-                            title="Temperature vs. Bike Share Count")
-st.plotly_chart(fig_temp_chart, use_container_width=True,
-                height=400, width=800)
+# Visualisasi dengan Plotly Express
+fig = px.bar(filtered_data, x='dteday', y='cnt', title='Jumlah total sewa sepeda untuk tahun 2011 selama musim dingin (musim 4)')
+fig.update_xaxes(title="Tanggal")
+fig.update_yaxes(title="Jumlah Sewa Sepeda")
+
+# Tampilkan plot menggunakan Streamlit
+st.plotly_chart(fig, use_container_width=True)
+
+
+
+st.subheader("Pertanyaan 2")
+# Filter tahun 2011, musim dingin (season 4), dan hari libur (holiday = 0)
+filtered_data = df_bsd_day[(df_bsd_day["yr"] == 0) & 
+                            (df_bsd_day["season"] == 4) & 
+                            (df_bsd_day["holiday"] == 1)]
+
+# Hitung jumlah total sepeda sewaan
+total_sepeda_sewaan = filtered_data["cnt"].sum()
+
+
+# Visualisasi dengan Plotly Express
+fig = px.bar(filtered_data, x='dteday', y='cnt',
+             title='Jumlah total sepeda sewaan per hari libur pada musim dingin tahun 2011')
+fig.update_xaxes(title="Tanggal")
+fig.update_yaxes(title="Jumlah Sewa Sepeda")
+
+# Tampilkan plot menggunakan Streamlit
+st.plotly_chart(fig, use_container_width=True)
+
+
+st.subheader("Pertanyaan 3")
+# Filter pengguna casual (casual) pada hari kerja (workingday = 1)
+filtered_data = df_bsd_day[(df_bsd_day["workingday"] == 1) & (df_bsd_day["casual"] > 0)]
+
+# Visualisasikan jumlah sewa sepeda casual pada hari kerja
+fig = px.bar(filtered_data, x="weekday", y="casual", title="Jumlah Sewa Sepeda Casual pada Hari Kerja")
+fig.update_xaxes(title="Hari Kerja")
+fig.update_yaxes(title="Jumlah Sewa Sepeda Casual")
+
+# Tampilkan plot menggunakan Streamlit
+st.plotly_chart(fig, use_container_width=True)
+
+
+st.subheader("Pertanyaan 4")
+# Filter musim gugur (season 3)
+filtered_data = df_bsd_day[df_bsd_day["season"] == 3]
+
+# Buat plot dengan Plotly untuk menganalisis pengaruh cuaca terhadap jumlah sewa sepeda
+fig = px.bar(filtered_data, x="weathersit", y="cnt", title="Pengaruh Cuaca terhadap Jumlah Sewa Sepeda (Musim Gugur)")
+fig.update_xaxes(title="Cuaca (weathersit)")
+fig.update_yaxes(title="Jumlah Sewa Sepeda (cnt)")
+
+# Tampilkan plot menggunakan Streamlit
+st.plotly_chart(fig, use_container_width=True)
+
+
+
+st.subheader("Pertanyaan 5")
+# Filter data tahun 2011 (yr = 0), Hari Natal (holiday = 0), dan musim dingin (season 4)
+filtered_data = df_bsd_hour[(df_bsd_hour["yr"] == 0) & 
+                             (df_bsd_hour["holiday"] == 0) & 
+                             (df_bsd_hour["season"] == 4)]
+
+# Hitung distribusi per jam sewa sepeda (cnt)
+distribusi_per_jam = filtered_data.groupby("hr")["cnt"].sum()
+
+# Visualisasi dengan Plotly Express
+fig = px.bar(x=distribusi_per_jam.index, y=distribusi_per_jam.values,
+             labels={'x': 'Jam', 'y': 'Jumlah Sewa Sepeda'},
+             title="Distribusi per Jam Sewa Sepeda pada Hari Natal tahun 2011 (Musim Dingin)")
+fig.update_xaxes(title="Jam")
+fig.update_yaxes(title="Jumlah Sewa Sepeda")
+
+# Tampilkan plot menggunakan Streamlit
+st.plotly_chart(fig, use_container_width=True)
